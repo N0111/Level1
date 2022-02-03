3cd6f6c8627a85e9614c93b80422cb769230ffbd
Nastya K <nastulya01112005@gmail.com>
2022-02-03 18:45:58 +0200

    Level1

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,31 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class MyScript : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+///    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+///    void Update()
+//    {
+
+
+
+//        //if (Input.GetKey(KeyCode.RightArrow))
+//        //    print("Right Arrow pressed");
+
+//        //if (Input.GetKey(KeyCode.D))
+//        //    print("D pressed");
+
+//        //if (Input.GetKeyDown(KeyCode.RightArrow))
+//        //    print("Right Arrow was pressed");
+
+//        //if (Input.GetKey(KeyCode.D))
+//        //    print("D pressed");
+//    }
+//}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 1f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+       var a = Input.GetAxis("Horizontal");
+       var b = Input.GetAxis("Vertical");
+
+       var direction = new Vector3(a, 0, b);
+
+       transform.position = transform.position + Speed * direction * Time.deltaTime;
+
+    }
+}
