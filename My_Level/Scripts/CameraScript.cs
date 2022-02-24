eae113a74f16e7c4e8ff4f86febe5d60cd7989e4
N0111 <95976517+N0111@users.noreply.github.com>
2022-02-24 18:07:41 +0200

    Add files via upload

--- My_Level/Scripts/CameraScript.cs
+++ My_Level/Scripts/CameraScript.cs
@@ -6,22 +6,23 @@ public class CameraScript : MonoBehaviour
 {
     [SerializeField] Transform Gamer;
     [SerializeField] Transform Camera;
-    [Range(0, 1)]
-    [SerializeField] float factor;
+    [SerializeField] float a;
+    //[Range(0, 1)]
+    //[SerializeField] float factor;
 
 
     private float speed = 2;
-    private Quaternion origRot;
-    private Quaternion cachedRot;
+    //private Quaternion origRot;
+    //private Quaternion cachedRot;
     void Start()
     {
-        origRot = Gamer.rotation;
-        cachedRot = Camera.rotation;
+        //origRot = Gamer.rotation;
+        //cachedRot = Camera.rotation;
     }
     void Update()
     {
-
-        Camera.rotation = Quaternion.Lerp(origRot, cachedRot, factor);
+        Camera.position = Vector3.MoveTowards(Camera.position, Gamer.position * a, speed * Time.deltaTime);
+        //Camera.rotation = Quaternion.Lerp(origRot, cachedRot, factor);
         //var dir = Gamer.position - Camera.position;
         //var ndir = Vector3.RotateTowards(Gamer.forward, dir, speed * Time.deltaTime, 0);
         //Camera.rotation = Quaternion.LookRotation(ndir);
